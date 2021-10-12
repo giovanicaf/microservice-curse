6ba4ec2b1f09ee816019bb2af82ba72e04243102
giovanicaf <giovanicaf@gmail.com>
2021-10-12 00:10:48 -0300

    RestTemplate

---
+++ hr-payroll/src/main/java/com/github/giovanicaf/hrpayroll/config/AppConfig.java
@@ -0,0 +1,14 @@
+package com.github.giovanicaf.hrpayroll.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+
+	@Bean
+	public RestTemplate restTemplete() {
+		return new RestTemplate();
+	}
+}
