fc3779660f986024161fb1f585b425ac52e01fe2
giovanicaf <giovanicaf@gmail.com>
2021-10-12 23:17:54 -0300

    Ribbon load balancing

--- hr-worker/src/main/java/com/github/giovanicaf/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/github/giovanicaf/hrworker/resources/WorkerResource.java
@@ -1,24 +1,28 @@
 package com.github.giovanicaf.hrworker.resources;
 
-import java.util.List;
-
+import com.github.giovanicaf.hrworker.entities.dto.WorkerDto;
+import com.github.giovanicaf.hrworker.repositories.WorkerRepository;
+import com.github.giovanicaf.hrworker.service.WorkerService;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.github.giovanicaf.hrworker.entities.dto.WorkerDto;
-import com.github.giovanicaf.hrworker.repositories.WorkerRepository;
-import com.github.giovanicaf.hrworker.service.WorkerService;
-
-import lombok.RequiredArgsConstructor;
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/workers")
 public class WorkerResource {
 
+	private static Logger logger = LoggerFactory.getLogger( WorkerResource.class );
+
+	public final Environment env;
 	public final WorkerRepository repository;
 	public final WorkerService service;
 
@@ -31,6 +35,8 @@ public class WorkerResource {
 
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<WorkerDto> findById(@PathVariable Long id){
+
+		logger.info( "PORT = " + env.getProperty("local.server.port") );
 
 		WorkerDto dto = service.findByWorker( repository.findById(id).get() );
 		return ResponseEntity.ok( dto );
