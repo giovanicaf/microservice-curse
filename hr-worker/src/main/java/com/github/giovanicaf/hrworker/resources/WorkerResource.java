1e4846c320fac131b308210f3da375f21845f42c
giovanicaf <giovanicaf@gmail.com>
2021-10-14 14:13:55 -0300

    Zuul configure timeout

--- hr-worker/src/main/java/com/github/giovanicaf/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/github/giovanicaf/hrworker/resources/WorkerResource.java
@@ -36,11 +36,11 @@ public class WorkerResource {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<WorkerDto> findById(@PathVariable Long id){
 
-//        try {
-//            Thread.sleep(3000L);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
+        try {
+            Thread.sleep(3000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         logger.info( "PORT = " + env.getProperty("local.server.port") );
 
 		WorkerDto dto = service.findByWorker( repository.findById(id).get() );
