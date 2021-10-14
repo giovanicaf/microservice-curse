2188c4a2cd79c731fd6be752fe541cd1789668ab
giovanicaf <giovanicaf@gmail.com>
2021-10-14 20:10:30 -0300

    Actuator for runtime config refresh

--- hr-worker/src/main/java/com/github/giovanicaf/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/github/giovanicaf/hrworker/resources/WorkerResource.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RefreshScope
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/workers")
